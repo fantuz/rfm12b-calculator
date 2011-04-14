4239d122ffe9f61ae646d39c8340a7daad7e4e41
miancuster@gmail.com <miancuster@gmail.com@21bf9218-077c-91c7-551b-74767b481a2d>
2011-04-14 20:28:12 +0000

    "Importing RFMB12 Command Calculator"

---
+++ RFMB12 Command Calculator/RFMB12 Command Calculator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RFMB12 Command Calculator
+//
+//  Created by Miron Iancu on 14.04.2011.
+//  Copyright 2011 home, baby, home. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
